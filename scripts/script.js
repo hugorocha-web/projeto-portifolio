4d2f569dd0e3c56481c7a3dffcc5ee58c831171a
hugorocha-web <hjj78092@gmail.com>
2026-08-28 12:04:58 -0300

    trocando icone lua por sol

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
 const btn = document.getElementById('btn')
 const body = document.querySelector('body')
-
+let icone = document.querySelector('#icone')
 
 btn.addEventListener('click', clicar)
 
-
-
-function clicar(){
+function clicar(event){
     body.classList.toggle('escuro')
-    console.log(body)
+    if (body.classList.contains('escuro')) {
+        icone.classList.replace('fa-moon', 'fa-sun');
+    } 
+    else {
+        icone.classList.replace('fa-sun', 'fa-moon');
+    }
+
 }
